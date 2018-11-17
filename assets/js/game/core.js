572f3ef0586a02ef8075362d53df2abba396f784
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2018-11-18 02:38:17 +0900

    v0.20.6.2-Alpha
    HotFix: 존재하지 않는 생성자에 접근하려는 버그를 해결하였습니다.

--- assets/js/game/core.js
+++ assets/js/game/core.js
@@ -1,5 +1,5 @@
 window.game = {
-  version: "v0.20.6.1-Alpha",
+  version: "v0.20.6.2-Alpha",
   masterVolume: 100,
   player: {},
   nowTurn: null,
--- assets/js/game/player.js
+++ assets/js/game/player.js
@@ -14,7 +14,6 @@ game.Player = class {
     this.deck = player.deck;
     this.hand = new game.Deck;
     this._luckyPoint_ = player.luckyPoint || 10;
-    this.crystal = player.crystal || new game.Crystal();
     this.isUseCrystal = false;
   }
 
